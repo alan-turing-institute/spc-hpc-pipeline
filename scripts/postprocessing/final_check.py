c73e95874a10b7cae2226d51fd94a3ee7f55cc81
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-07-11 14:08:28 +0100

    Refine final_check script

--- scripts/postprocessing/final_check.py
+++ scripts/postprocessing/final_check.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from argparse import ArgumentParser
 import glob
 import os
 from pathlib import Path
 from typing import List
-
+from functools import reduce
 import pandas as pd
 
 
@@ -40,60 +41,63 @@ def get_file_list(paths: List[str]) -> pd.DataFrame:
     return df_files
 
 
-# Years to consider outputs
-YEARS = [2012, 2020, 2022, 2032, 2039]
+def main(args: ArgumentParser):
+    # Years to consider outputs
+    YEARS = [2012, 2020, 2022, 2032, 2039]
 
-# LAD 2020 concat for GB
-new_lad_list = pd.concat(
-    [
-        pd.read_csv("data/new_lad_list_England.csv"),
-        pd.read_csv("data/new_lad_list_Wales.csv"),
-        pd.read_csv("data/new_lad_list_Scotland.csv"),
-    ]
-)
+    # LAD 2020 concat for GB
+    new_lad_list = pd.concat(
+        [
+            pd.read_csv("data/new_lad_list_England.csv"),
+            pd.read_csv("data/new_lad_list_Wales.csv"),
+            pd.read_csv("data/new_lad_list_Scotland.csv"),
+        ]
+    )
 
+    # Get file list
+    df_files = get_file_list(args.paths)
 
-# Output paths
-data_paths = [
-    "/mnt/vmfileshare/SPENSER_Outputs/outputs/",
-    "/mnt/vmfileshare/SPENSER_missing_lads/outputs/",
-]
-regions = ["England", "Wales", "Scotland"]
+    # Combine with new_lad list
+    combined = new_lad_list.merge(
+        df_files, left_on="LAD20CD", right_on="region", how="left"
+    )
 
-# Get file list
-df_files = get_file_list(data_paths)
+    # Only consider the assigned outputs
+    combined = combined[combined["type"].str.startswith("ass")].reset_index(drop=True)
 
+    # Print full dataframe
+    print(combined.to_markdown())
 
-# Combine with new_lad list
-combined = new_lad_list.merge(
-    df_files, left_on="LAD20CD", right_on="region", how="left"
-)
+    # Check none are missing
+    assert combined[combined["full_path"].isna()].shape[0] == 0
 
+    # All outputs are present: 5 years and 2 file types per region
+    assert new_lad_list.shape[0] * len(YEARS) * 2 == combined.shape[0]
 
-# Only consider the assigned outputs
-combined = combined[combined["type"].str.startswith("ass")].reset_index(drop=True)
+    # Check non-empty
+    assert ~combined["full_path"].map(os.path.getsize).eq(0).any()
 
-# Print full dataframe
-print(combined.to_markdown())
+    # Check regions are distinct
+    dfs_files = [get_file_list(path) for path in args.paths]
+    assert (
+        len(
+            reduce(
+                lambda x, y: x & y,
+                [set(df_files["region"].to_list()) for df_files in dfs_files],
+            )
+        )
+        == 0
+    )
 
-# Check none are missing
-assert combined[combined["full_path"].isna()].shape[0] == 0
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--paths",
+        type=str,
+        nargs="+",
+    )
+    args, unknown = parser.parse_known_args()
 
+    print(args)
 
-# All outputs are present: 5 years and 2 file types per region
-assert new_lad_list.shape[0] * 5 * 2 == combined.shape[0]
-
-
-# Check non-empty
-assert ~combined["full_path"].map(os.path.getsize).eq(0).any()
-
-
-# Check regions are distinct
-df_files_1 = get_file_list(data_paths[:1])
-df_files_2 = get_file_list(data_paths[1:])
-assert (
-    len(set(df_files_1["region"].to_list()) & set(df_files_2["region"].to_list())) == 0
-)
-
-# Print remaining regions as space sep string
-print(" ".join(df_files_2["region"].drop_duplicates().to_list()))
+    main(args)
