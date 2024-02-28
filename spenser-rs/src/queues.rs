13605522d207ae0596bce364db78432ac2c94a76
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-02-28 17:51:00 +0000

    Refactor queues

--- spenser-rs/src/queues.rs
+++ spenser-rs/src/queues.rs
@@ -74,21 +74,44 @@ fn get_closest(age: Age, v: &Vec<PID>, p_data: &TiVec<PID, Person>) -> Option<(u
     }
 }
 
-macro_rules! shuffle {
-    ($v:ident, $rng:ident) => {
-        $v.iter_mut().for_each(|(_, v)| v.shuffle($rng));
-    };
+/// Additional operations useful for maps with queues.
+trait QueueOperations<K: Ord> {
+    /// Add a given `pid` to queue at a given key.
+    fn add_pid(&mut self, key: K, pid: PID);
+    /// Shuffle a given queue.
+    fn shuffle(&mut self, rng: &mut StdRng);
+    /// Get an unmatched `pid` given a key and update matched and unmatched sets.
+    fn get_sample(
+        &mut self,
+        key: &K,
+        matched: &mut HashSet<PID>,
+        unmatched: &mut HashSet<PID>,
+    ) -> Option<PID>;
 }
 
-/// Given an MSOA, return a PID if one exists and update matched and unmatched sets.
-fn get_sample(
-    map: &mut BTreeMap<MSOA, Vec<PID>>,
-    msoa: &MSOA,
-    matched: &mut HashSet<PID>,
-    unmatched: &mut HashSet<PID>,
-) -> Option<PID> {
-    let v = map.get_mut(msoa).expect("Invalid MSOA.");
-    update_pid_vec(v, matched, unmatched)
+impl<K: Ord> QueueOperations<K> for BTreeMap<K, Vec<PID>> {
+    fn add_pid(&mut self, key: K, pid: PID) {
+        {
+            self.entry(key)
+                .and_modify(|el| {
+                    el.push(pid);
+                })
+                .or_insert(vec![pid]);
+        }
+    }
+    fn shuffle(&mut self, rng: &mut StdRng) {
+        self.iter_mut().for_each(|(_, v)| v.shuffle(rng))
+    }
+    /// Given an MSOA, return a PID if one exists and update matched and unmatched sets.
+    fn get_sample(
+        &mut self,
+        key: &K,
+        matched: &mut HashSet<PID>,
+        unmatched: &mut HashSet<PID>,
+    ) -> Option<PID> {
+        let v = self.get_mut(key).expect("Invalid MSOA.");
+        update_pid_vec(v, matched, unmatched)
+    }
 }
 
 impl Queues {
@@ -115,87 +138,40 @@ impl Queues {
         let mut people_by_area_over_75: BTreeMap<MSOA, Vec<PID>> = BTreeMap::new();
         let mut people_by_area_19_to_25: BTreeMap<MSOA, Vec<PID>> = BTreeMap::new();
         let mut people_by_area_over_16: BTreeMap<MSOA, Vec<PID>> = BTreeMap::new();
-        p_data.iter_enumerated().for_each(|(idx, person)| {
-            let area = person.msoa.to_owned();
-            let age = person.age;
-            let sex = person.sex;
-            let eth = person.eth;
-            people_by_area_ase
-                .entry((area.clone(), age, sex, eth))
-                .and_modify(|el| {
-                    el.push(idx);
-                })
-                .or_insert(vec![idx]);
+        p_data.iter_enumerated().for_each(|(pid, person)| {
+            let (area, age, sex, eth) =
+                (person.msoa.to_owned(), person.age, person.sex, person.eth);
+            // Add PID to relevent queues
+            people_by_area_ase.add_pid((area.clone(), age, sex, eth), pid);
             if age > Age(75) {
-                people_by_area_over_75
-                    .entry(area.clone())
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
+                people_by_area_over_75.add_pid(area.clone(), pid);
             }
             if age > Age(18) && age < Age(26) {
-                people_by_area_19_to_25
-                    .entry(area.clone())
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
+                people_by_area_19_to_25.add_pid(area.clone(), pid);
             }
             if age > Age(16) {
-                people_by_area_over_16
-                    .entry(area.clone())
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
+                people_by_area_over_16.add_pid(area.clone(), pid);
             }
-
             if age > ADULT_AGE {
-                adults_by_area_se
-                    .entry((area.clone(), sex, eth))
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
-                adults_by_area_s
-                    .entry((area.clone(), sex))
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
-                adults_by_area
-                    .entry(area.clone())
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
+                adults_by_area_se.add_pid((area.clone(), sex, eth), pid);
+                adults_by_area_s.add_pid((area.clone(), sex), pid);
+                adults_by_area.add_pid(area.clone(), pid);
             } else {
-                children_by_area_se
-                    .entry((area.clone(), sex, eth))
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
-                children_by_area_s
-                    .entry((area.clone(), sex))
-                    .and_modify(|el| {
-                        el.push(idx);
-                    })
-                    .or_insert(vec![idx]);
+                children_by_area_se.add_pid((area.clone(), sex, eth), pid);
+                children_by_area_s.add_pid((area.clone(), sex), pid);
             }
         });
 
         // Shuffle queues
-        shuffle!(people_by_area_ase, rng);
-        shuffle!(adults_by_area_se, rng);
-        shuffle!(adults_by_area_s, rng);
-        shuffle!(adults_by_area, rng);
-        shuffle!(children_by_area_se, rng);
-        shuffle!(children_by_area_s, rng);
-        shuffle!(people_by_area_over_75, rng);
-        shuffle!(people_by_area_19_to_25, rng);
-        shuffle!(people_by_area_over_16, rng);
+        people_by_area_ase.shuffle(rng);
+        adults_by_area_se.shuffle(rng);
+        adults_by_area_s.shuffle(rng);
+        adults_by_area.shuffle(rng);
+        children_by_area_se.shuffle(rng);
+        children_by_area_s.shuffle(rng);
+        people_by_area_over_75.shuffle(rng);
+        people_by_area_19_to_25.shuffle(rng);
+        people_by_area_over_16.shuffle(rng);
 
         Self {
             people_by_area_ase,
@@ -213,28 +189,16 @@ impl Queues {
     }
 
     pub fn sample_person_over_75(&mut self, msoa: &MSOA) -> Option<PID> {
-        get_sample(
-            &mut self.people_by_area_over_75,
-            msoa,
-            &mut self.matched,
-            &mut self.unmatched,
-        )
+        self.people_by_area_over_75
+            .get_sample(msoa, &mut self.matched, &mut self.unmatched)
     }
     pub fn sample_person_over_16(&mut self, msoa: &MSOA) -> Option<PID> {
-        get_sample(
-            &mut self.people_by_area_over_16,
-            msoa,
-            &mut self.matched,
-            &mut self.unmatched,
-        )
+        self.people_by_area_over_16
+            .get_sample(msoa, &mut self.matched, &mut self.unmatched)
     }
     pub fn sample_person_19_to_25(&mut self, msoa: &MSOA) -> Option<PID> {
-        get_sample(
-            &mut self.people_by_area_19_to_25,
-            msoa,
-            &mut self.matched,
-            &mut self.unmatched,
-        )
+        self.people_by_area_19_to_25
+            .get_sample(msoa, &mut self.matched, &mut self.unmatched)
     }
 
     pub fn sample_adult_any(&mut self, msoa: &MSOA) -> Option<PID> {
